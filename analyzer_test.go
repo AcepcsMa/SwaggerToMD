847126039f12dabb249e38745586fce53dd5fb57
haoxiangma <mahomehi@126.com>
2018-12-17 15:07:40 +0800

    test ExtractAPIs and related structs

--- analyzer_test.go
+++ analyzer_test.go
@@ -3,32 +3,33 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"testing"
 )
 
 // test AnalyzeOverview in SwaggerAnalyzer
-//func TestSwaggerAnalyzer_AnalyzeOverview(t *testing.T) {
-//	t.Log("Test swagger analyzer - AnalyzeOverview")
-//	{
-//		model := Model{}
-//		t.Log("Generate a swagger model")
-//		{
-//			fin, err := os.Open("test.json")
-//			defer fin.Close()
-//			if err != nil {
-//				t.Fatal(err)
-//			}
-//			json.NewDecoder(fin).Decode(&model)
-//		}
-//		analyzer := NewSwaggerAnalyzer(ENGLISH)
-//		overviewContent, err := analyzer.AnalyzeOverview(model)
-//		if err != nil {
-//			log.Fatal(err)
-//		}
-//		fmt.Println(overviewContent)
-//	}
-//}
+func TestSwaggerAnalyzer_AnalyzeOverview(t *testing.T) {
+	t.Log("Test swagger analyzer - AnalyzeOverview")
+	{
+		model := Model{}
+		t.Log("Generate a swagger model")
+		{
+			fin, err := os.Open("test.json")
+			defer fin.Close()
+			if err != nil {
+				t.Fatal(err)
+			}
+			json.NewDecoder(fin).Decode(&model)
+		}
+		analyzer := NewSwaggerAnalyzer(ENGLISH)
+		overviewContent, err := analyzer.AnalyzeOverview(model)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(overviewContent)
+	}
+}
 
 func TestSwaggerAnalyzer_ExtractAPIs(t *testing.T) {
 	t.Log("Test swagger analyzer - ExtractAPIs")
